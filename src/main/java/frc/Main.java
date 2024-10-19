a96c2533ddd6c32949f79fbf26a5c975e908f966
Kay <kay.kasemir@gmail.com>
2024-10-18 20:56:37 -0400

    FirstDemoRobot

--- src/main/java/frc/Main.java
+++ src/main/java/frc/Main.java
@@ -13,20 +13,21 @@ public final class Main
   public static void main(String... args)
   {
     // System.out.println("Hello, robot!");
-    // RobotBase.startRobot(frc.tools.CommandRobotBase::new);
+    
+    RobotBase.startRobot(frc.demo.FirstRobotDemo::new);
     // RobotBase.startRobot(frc.demo.MotorPhoenix6DemoRobot::new);
     // RobotBase.startRobot(() -> new frc.swervelib.DriverDemoRobot(new frc.swervebot.Driver(0)));
     // RobotBase.startRobot(() -> new frc.swervelib.RotatorDemoRobot(new frc.swervebot.Rotator(0, 0)));
 
-    RobotBase.startRobot(() ->
-      new frc.swervelib.SwerveModuleDemoRobot(
-        new SwerveModule[]
-        {
-          new SwerveModule(new frc.swervebot.Rotator(0,  -17), new frc.swervebot.Driver(0)),
-          new SwerveModule(new frc.swervebot.Rotator(1,  -89+180), new frc.swervebot.Driver(1)),
-          new SwerveModule(new frc.swervebot.Rotator(2,   20.8+180), new frc.swervebot.Driver(2)),
-          new SwerveModule(new frc.swervebot.Rotator(3, -106), new frc.swervebot.Driver(3))
-        }));
+    // RobotBase.startRobot(() ->
+    //   new frc.swervelib.SwerveModuleDemoRobot(
+    //     new SwerveModule[]
+    //     {
+    //       new SwerveModule(new frc.swervebot.Rotator(0,  -17), new frc.swervebot.Driver(0)),
+    //       new SwerveModule(new frc.swervebot.Rotator(1,  -89+180), new frc.swervebot.Driver(1)),
+    //       new SwerveModule(new frc.swervebot.Rotator(2,   20.8+180), new frc.swervebot.Driver(2)),
+    //       new SwerveModule(new frc.swervebot.Rotator(3, -106), new frc.swervebot.Driver(3))
+    //     }));
 
     // RobotBase.startRobot(frc.swervebot.SwerveBot::new);
   }
