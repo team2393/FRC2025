8724c735b9dbc169159c08962da2a71fa2c53ceb
Kay <kay.kasemir@gmail.com>
2024-11-16 18:09:31 -0500

    Start robot drivetrain

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,27 +7,27 @@ package frc.robot;
 /** Robot (Hardware) Map: What's connected where? */
 public class RobotMap
 {
-  // Use comments for port and fuse on the power distribution port
+  // Use comments for port and fuse on the power distribution panel
   // Constants define CAN IDs or RoboRIO ports
 
   // RoboRIO: 10 Amp, port 21
   // Radio  : 10 Amp, port 20
 
-  // SparkMax(?) drive motors: 40 Amp Fuse
-  public static final int FRONT_LEFT_DRIVE  = 9;   // Port 10
-  public static final int FRONT_RIGHT_DRIVE = 8;   // Port 11
-  public static final int BACK_RIGHT_DRIVE  = 2;   // Port 12
-  public static final int BACK_LEFT_DRIVE   = 3;   // Port 13
+  // Kraken drive motors: 40 Amp Fuse
+  public static final int FRONT_LEFT_DRIVE  = 5;   // Port 10
+  public static final int FRONT_RIGHT_DRIVE = 6;   // Port 11
+  public static final int BACK_RIGHT_DRIVE  = 7;   // Port 12
+  public static final int BACK_LEFT_DRIVE   = 8;   // Port 13
 
-  // SparkMax(?) rotator motors: 40 Amp Fuse
-  public static final int FRONT_LEFT_ROTATE  = 7;   // Port 9
-  public static final int FRONT_RIGHT_ROTATE = 10;  // Port 8
-  public static final int BACK_RIGHT_ROTATE  = 4;   // Port 7
-  public static final int BACK_LEFT_ROTATE   = 1;   // Port 6
+  // Kraken rotator motors: 40 Amp Fuse
+  public static final int FRONT_LEFT_ROTATE  = 1;   // Port 9
+  public static final int FRONT_RIGHT_ROTATE = 2;  // Port 8
+  public static final int BACK_RIGHT_ROTATE  = 3;   // Port 7
+  public static final int BACK_LEFT_ROTATE   = 4;   // Port 6
 
-  // CANCoder(?) angle sensors: 10 Amp Fuse
-  public static final int FRONT_LEFT_ANGLE  = 11;   // Port 3
-  public static final int FRONT_RIGHT_ANGLE = 12;   // Port 2
-  public static final int BACK_RIGHT_ANGLE  = 6;    // Port 1
-  public static final int BACK_LEFT_ANGLE   = 5;    // Port 0
+  // CANCoder angle sensors: 10 Amp Fuse
+  public static final int FRONT_LEFT_ANGLE  = 1;   // Port 3
+  public static final int FRONT_RIGHT_ANGLE = 2;   // Port 2
+  public static final int BACK_RIGHT_ANGLE  = 3;    // Port 1
+  public static final int BACK_LEFT_ANGLE   = 4;    // Port 0
 }
---
+++ src/main/java/frc/robot/Rotator.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST Team 2393 and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot;
+
+import com.ctre.phoenix6.configs.OpenLoopRampsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import frc.swervelib.RotatorBase;
+
+/** SwerveBot Rotator using Kraken and CANcoder */
+public class Rotator extends RotatorBase
+{
+  // TODO Calibrate
+  private final double DEG_PER_TURN = 1.0;
+  private final TalonFX motor;
+  private final CANcoder encoder;
+
+  /** Construct Rotator
+   *  @param index Rotator index 0..3
+   *  @param motor_id Motor CAN id
+   *  @param encoder_id Encoder CAN id
+   *  @param offset Offset from 'forward' in degrees
+   */
+  public Rotator(int index, int motor_id, int encoder_id, double offset)
+  {
+    // TODO Find PID settings
+    super(index, offset, 0.1, 0.4, 0.01, 0.01, 9.0);
+    motor = new TalonFX(motor_id);
+    TalonFXConfiguration config = new TalonFXConfiguration()
+        .withOpenLoopRamps(new OpenLoopRampsConfigs().withVoltageOpenLoopRampPeriod(0.3));
+    motor.getConfigurator().apply(config);    
+    motor.clearStickyFaults();
+    motor.setNeutralMode(NeutralModeValue.Coast);
+
+    encoder = new CANcoder(encoder_id);
+    encoder.clearStickyFaults();
+  }
+
+  @Override
+  public double getRawDegrees()
+  {
+    return encoder.getAbsolutePosition().getValueAsDouble() * DEG_PER_TURN;
+  }
+
+  @Override
+  public void setVoltage(double voltage)
+  {
+    motor.setVoltage(voltage);
+  }
+}
