54fe10fbfdf7ce1576e0a2a576e1f1e831c30a5c
Kay <kay.kasemir@gmail.com>
2025-03-01 16:20:44 -0500

    Show gamepiece on dashboard

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -2,24 +2,30 @@ package frc.robot;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase
 {
     private final TalonFX motor;
     private final DigitalInput sensor;
+    private final NetworkTableEntry nt_gamepiece;
 
     public Intake()
     {
         motor = new TalonFX(RobotMap.INTAKE);
         sensor = new DigitalInput(RobotMap.INTAKE_SENSOR);
+        nt_gamepiece = SmartDashboard.getEntry("Gamepiece");
     }
 
     public boolean hasGamePiece()
     {
         // TODO: Check polarity
-        return sensor.get();
+        boolean have_gamepiece = sensor.get();
+        nt_gamepiece.setBoolean(have_gamepiece);
+        return have_gamepiece;
     }
 
     public void setVoltage(double voltage)
