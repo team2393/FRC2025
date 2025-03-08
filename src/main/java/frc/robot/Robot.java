ca1ae109f9b075be5c1c8d462adfe32021fc1253
kasparwinston <winstonkr@icloud.com>
2025-03-08 09:52:53 -0500

    speed

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -49,8 +49,8 @@ public class Robot extends CommandRobotBase
   public Robot()
   {
     // Configure speed
-    SwerveOI.MAX_METERS_PER_SEC = SwerveDrivetrain.MAX_METERS_PER_SEC = 3.0; // 1 .. 3
-    SwerveOI.MAX_ROTATION_DEG_PER_SEC = SwerveDrivetrain.MAX_ROTATION_DEG_PER_SEC = 45;
+    SwerveOI.MAX_METERS_PER_SEC = SwerveDrivetrain.MAX_METERS_PER_SEC = 2.0; // 1 .. 3
+    SwerveOI.MAX_ROTATION_DEG_PER_SEC = SwerveDrivetrain.MAX_ROTATION_DEG_PER_SEC = 180;
     SwerveOI.forward_slew = new SlewRateLimiter(SwerveOI.MAX_METERS_PER_SEC);
     SwerveOI.side_slew = new SlewRateLimiter(SwerveOI.MAX_METERS_PER_SEC);
     SwerveOI.rotation_slew = new SlewRateLimiter(45);
