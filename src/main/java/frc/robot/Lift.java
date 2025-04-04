2aecc7edd563e4505fc063e2bc1a8d99be19b8ab
kasparwinston <winstonkr@icloud.com>
2025-04-04 08:29:25 -0400

    increase lift speed

--- src/main/java/frc/robot/Lift.java
+++ src/main/java/frc/robot/Lift.java
@@ -64,7 +64,7 @@ public class Lift extends SubsystemBase
   /** PID */
   // private PIDController pid = new PIDController(17, 10, 0);
   private ProfiledPIDController pid = new ProfiledPIDController(17, 10, 0,
-                                            new Constraints(4,4));
+                                            new Constraints(5, 5));
 
   private double simulated_height = 0.0;
 
